ca78db951c9211b9eeca69014f697403eb8a66dd
ricardo-ave-gonzalez <ricardo.ave.gonzalez@gmail.com>
2022-02-13 23:08:01 -0300

    min max y promedio

--- src/main/java/com/example/demo/exercises/Estadisticas.java
+++ src/main/java/com/example/demo/exercises/Estadisticas.java
@@ -16,17 +16,37 @@ public class Estadisticas {
         System.out.println("Se registraron " + count + " vehículos");
     }
 
-    public static void autosMinimo() throws Exception {
+    public static void autosPrecioMinimo() throws Exception {
         List<Auto> autos = MockData.obtenerAutos();
         double min = autos.stream()
                 .mapToDouble(z -> z.precio)
                 .min()
                 .orElse(0);
-        System.out.println(min);
+        System.out.println("El vehículo mas económico cuesta $" + min);
+    }
+
+    public static void autosPrecioMaximo() throws Exception{
+        List<Auto> autos = MockData.obtenerAutos();
+        double max = autos.stream()
+                .mapToDouble(z -> z.precio)
+                .max()
+                .orElse(0);
+        System.out.println("El vehículo mas caro cuesta $" + max);
+    }
+
+    public static void autosPrecioPromedio() throws Exception {
+        List<Auto> autos = MockData.obtenerAutos();
+        double promedio = autos.stream()
+                .mapToDouble(z -> z.precio)
+                .average()
+                .orElse(0);
+        System.out.println("El precio promedio es de " + promedio);
     }
 
     public static void main(String[] args) throws Exception {
         autosCount();
-        autosMinimo();
+        autosPrecioMinimo();
+        autosPrecioMaximo();
+        autosPrecioPromedio();
     }
 }
