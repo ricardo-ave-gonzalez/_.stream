da9b3c841ab6aec43c2d16f298463c5dc062fce9
ricardo-ave-gonzalez <ricardo.ave.gonzalez@gmail.com>
2022-02-26 20:48:36 -0300

    merge version commiteada

---
+++ src/main/java/com/example/demo/exercises/Estadisticas.java
@@ -0,0 +1,76 @@
+package com.example.demo.exercises;
+
+import com.example.demo.mockdata.MockData;
+import com.example.demo.models.Auto;
+
+import java.math.BigDecimal;
+import java.util.DoubleSummaryStatistics;
+import java.util.List;
+
+public class Estadisticas {
+
+    public static void autosCountConFiltro() throws Exception {
+        List<Auto> autos = MockData.obtenerAutos();
+        long count = autos.stream()
+                .filter(z -> z.marca.equals("Ford"))
+                .filter(z -> z.año > 2010)
+                .count();
+        System.out.println("Se registraron " + count + " vehículos marca Ford modelo 2010 o superior");
+    }
+
+    public static void autosPrecioMinimo() throws Exception {
+        List<Auto> autos = MockData.obtenerAutos();
+        double min = autos.stream()
+                .mapToDouble(z -> z.precio)
+                .min()
+                .orElse(0);
+        System.out.println("El vehículo mas económico cuesta $" + min);
+    }
+
+    public static void autosPrecioMaximo() throws Exception{
+        List<Auto> autos = MockData.obtenerAutos();
+        double max = autos.stream()
+                .mapToDouble(z -> z.precio)
+                .max()
+                .orElse(0);
+        System.out.println("El vehículo mas caro cuesta $" + max);
+    }
+
+    public static void autosPrecioPromedio() throws Exception {
+        List<Auto> autos = MockData.obtenerAutos();
+        double promedio = autos.stream()
+                .mapToDouble(z -> z.precio)
+                .average()
+                .orElse(0);
+        System.out.println("El precio promedio es de " + promedio);
+    }
+
+    public static void autosPrecioTotal() throws Exception{
+        List<Auto> autos = MockData.obtenerAutos();
+        double total = autos.stream()
+                .mapToDouble(z -> z.precio)
+                .sum();
+        System.out.println("La suma total de todos los vehículos es de " + total);
+    }
+
+    public static void autosPrecioEstadisticas() throws Exception {
+        List<Auto> autos = MockData.obtenerAutos();
+        DoubleSummaryStatistics statistics = autos.stream()
+                .mapToDouble(z -> z.precio)
+                .summaryStatistics();
+        System.out.println(statistics.getCount());
+        System.out.println(statistics.getMin());
+        System.out.println(statistics.getMax());
+        System.out.println(statistics.getAverage());
+        System.out.println(BigDecimal.valueOf(statistics.getSum()));
+    }
+
+    public static void main(String[] args) throws Exception {
+        autosCountConFiltro();
+        autosPrecioMinimo();
+        autosPrecioMaximo();
+        autosPrecioPromedio();
+        autosPrecioTotal();
+        autosPrecioEstadisticas();
+    }
+}
---
+++ src/main/java/com/example/demo/exercises/Filtracion.java
@@ -0,0 +1,40 @@
+package com.example.demo.exercises;
+
+import com.example.demo.mockdata.MockData;
+import com.example.demo.models.Auto;;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Filtracion {
+
+    public static void dropWhile() throws Exception {
+        System.out.println("usando filter");
+        Stream.of(2, 4, 6, 8, 9, 10, 12).filter(n -> n % 2 == 0)
+                .forEach(n -> System.out.print(n + " "));
+        System.out.println();
+        System.out.println("using dropWhile");
+        Stream.of(2, 4, 6, 8, 9, 10, 12).dropWhile(n -> n % 2 == 0)
+                .forEach(n -> System.out.print(n + " "));
+
+    }
+
+    public static void autosFiltrados() throws Exception {
+        List<Auto> autos = MockData.obtenerAutos();
+        Predicate<Auto> autoPredicate = z -> z.precio < 20000.00;
+        Predicate<Auto> yellow = z -> z.color.equals("Yellow");
+
+        List<Auto> autosFiltrados = autos.stream()
+                .filter(autoPredicate)
+                .filter(yellow)
+                .collect(Collectors.toList());
+
+        autosFiltrados.forEach(System.out::println);
+    }
+
+    public static void main(String[] args) throws Exception {
+        autosFiltrados();
+        //dropWhile();
+    }
+}
