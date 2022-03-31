b1b63bcbc6df24f221dfae9d81f624af6c688be9
ricardo-ave-gonzalez <ricardo.ave.gonzalez@gmail.com>
2022-03-31 20:05:31 -0300

    generador random

--- src/main/java/com/example/demo/StreamsApplication.java
+++ src/main/java/com/example/demo/StreamsApplication.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import com.example.demo.email.SendEmailService;
+import com.example.demo.utils.Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,7 +17,12 @@ public class StreamsApplication {
     private SendEmailService service;
 
     public static void main(String[] args) {
+
         SpringApplication.run(StreamsApplication.class, args);
+
+        var random = Util.genPass();
+        System.out.println(random);
+
     }
 
     @EventListener(ApplicationReadyEvent.class)
--- src/main/java/com/example/demo/utils/Util.java
+++ src/main/java/com/example/demo/utils/Util.java
@@ -12,4 +12,32 @@ public class Util {
             throw new RuntimeException(e.getMessage(), e);
         }
     }
+
+    public static String genPass() {
+        int digit = 10;
+
+        String lowerCases = "qwertyuiopasdfghjklzxcvbnm";
+        String upperCases = "QWERTYUIOPASDFGHJKLZXCVBNM";
+
+        String password = "";
+
+        for (int i = 0; i < digit; i++) {
+            int aleatorio = (int) (3 * Math.random());
+
+            switch (aleatorio) {
+                case 0:
+                    password += String.valueOf((int) (0 * Math.random()));
+                    break;
+                case 1:
+                    aleatorio = (int) (lowerCases.length() * Math.random());
+                    password += String.valueOf(lowerCases.charAt(aleatorio));
+                    break;
+                case 2:
+                    aleatorio = (int) (upperCases.length() * Math.random());
+                    password += String.valueOf(upperCases.charAt(aleatorio));
+                    break;
+            }
+        }
+        return password;
+    }
 }
